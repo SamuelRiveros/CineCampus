d66664161af4406d32f68213b9778c3d3a1dde56
SamuelRiveros <156517817+SamuelRiveros@users.noreply.github.com>
2024-07-29 10:11:47 -0500

    feat: :sparkles: Gestion de roles y usuarios funcional
    feat: :sparkles: Avances en creación de usuarios y roles
    
    d39b4a11053c20fa283fae3821e90ce153f0dbb1
    
    feat: :memo: avances simples en documentacion
    
    249350ae93c3fade5c3de290df02762d0025041c
    
    feat: :sparkles: Gestion de roles y usuarios agregada y funcional
    
    850e0e4f4927530d4c4f8f8a0715a765fd37e50b

--- js/modules/cliente.js
+++ js/modules/cliente.js
@@ -1,5 +1,4 @@
 import { connect } from "../../connect.js";
-import { ObjectId } from "mongodb";
 
 export class Clientes extends connect {
     constructor() {
@@ -11,4 +10,59 @@ export class Clientes extends connect {
         Clientes.instance = this;
         return this;
     }
-}
+
+    generateRandomPassword(length) {
+        const charset = "abcdefg";
+        let password = "";
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * charset.length);
+            password += charset[randomIndex];
+        }
+        return password;
+    }
+
+    async createClientAndUser() { 
+        try {
+            const clientData = {
+                nombre: "Miguel Castro",
+                telefono: 3234515699,
+                email: "miguel.castro@gmail.com",
+                targeta_vip: true
+            };
+
+            const existingCliente = await this.collection.findOne({
+                nombre: clientData.nombre,
+                telefono: clientData.telefono,
+                email: clientData.email,
+                targeta_vip: clientData.targeta_vip
+            });
+
+            if (existingCliente) {
+                console.log("El usuario ya existe:");
+                return existingCliente;
+            } else {
+                await this.collection.insertOne(clientData);
+                console.log("Cliente ingresado");
+            }
+            
+            // Creación del usuario
+            const password = this.generateRandomPassword(8); // Contraseña de 8 caracteres
+            const rol = clientData.targeta_vip ? "usuarioVip" : "usuarioEstandar";
+
+            await this.db.command({
+                createUser: clientData.email,
+                pwd: password,
+                roles: [
+                  { role: rol, db: "CineCampus" }
+                ]
+            });
+
+            console.log(`Usuario creado. Contraseña del usuario: ${password}`);
+        } catch (error) {
+            console.log(`Error al insertar cliente: ${error}`);
+        } finally {
+            await this.close();
+        }
+    }
+
+    }
