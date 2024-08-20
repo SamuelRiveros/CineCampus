26d44756ee6589b6584eb79034f829cca6a213c7
SamuelRiveros <156517817+SamuelRiveros@users.noreply.github.com>
2024-08-20 13:17:13 -0500

    feat: :sparkles: Creación de entorno de Vue.js para el Front-End

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<!-- src/App.vue -->
+<template>
+    <div id="app">
+      <h1>Hello, Vue is working!</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App'
+  }
+  </script>
+  
+  <style>
+  /* Puedes agregar estilos aquí si lo deseas */
+  </style>
+  
---
+++ src/main.js
@@ -0,0 +1,5 @@
+// src/main.js
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
